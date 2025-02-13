4b8d0d731bb8d140046766c4cab7b6dcd28e05c0
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-02-13 20:18:55 +0200

    TabBarController with 3 tabs, Search, Home, Library

--- Spotify/Controllers/Core/HomeViewController.swift
+++ Spotify/Controllers/Core/HomeViewController.swift
@@ -11,8 +11,6 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Home"
-        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
 
--- Spotify/Controllers/Core/TabBarViewController.swift
+++ Spotify/Controllers/Core/TabBarViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupUI()
         
     }
     
-
+    // MARK: - SetupUI
+    private func setupUI() {
+        setupTabBar()
+    }
+    
+    private func setupTabBar() {
+        let homeNav    = createNavBarController(for: HomeViewController(), titleDisplayMode: .always, titled: "Home")
+        let searchNav  = createNavBarController(for: SearchViewController(), titleDisplayMode: .always, titled: "Search")
+        let libraryNav = createNavBarController(for: LibraryViewController(), titleDisplayMode: .always, titled: "Library")
+        
+        homeNav.navigationBar.prefersLargeTitles    = true
+        searchNav.navigationBar.prefersLargeTitles  = true
+        libraryNav.navigationBar.prefersLargeTitles = true
+        
+        homeNav.tabBarItem    = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
+        searchNav.tabBarItem  = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        libraryNav.tabBarItem = UITabBarItem(title: "Library", image: UIImage(systemName: "books.vertical"), tag: 1)
+        
+        
+        viewControllers = [homeNav, searchNav, libraryNav]
+    }
+    
+    private func createNavBarController(for rootViewController: UIViewController,  titleDisplayMode: UINavigationItem.LargeTitleDisplayMode, titled: String) -> UINavigationController {
+        rootViewController.title = titled
+        rootViewController.navigationItem.largeTitleDisplayMode = titleDisplayMode
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.navigationBar.prefersLargeTitles = true
+        return navController
+    }
 }
