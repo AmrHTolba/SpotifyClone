a0d754f6eb8f93746579446fec66749cf936b601
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-02-15 15:43:09 +0200

    Created Signin button that navigates to the to Auth VC

--- Spotify/Controllers/Starting Screen/WelcomeViewController.swift
+++ Spotify/Controllers/Starting Screen/WelcomeViewController.swift
@@ -8,6 +8,19 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - UI Elements
+    private let signInButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Sign In with Spotify", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .white
+        return button
+    }()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -16,12 +29,38 @@ class WelcomeViewController: UIViewController {
         print(AuthManager.clientID)
         print(AuthManager.clientSecret)
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
 
     
     // MARK: - Setup UI
     private func setupUI() {
         self.title = "Spotify"
         self.view.backgroundColor = .systemGreen
+        setupButton()
+    }
+    
+    private func setupButton() {
+        view.addSubview(signInButton)
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        signInButton.frame = CGRect(
+            x: 20,
+            y: view.height-100-view.safeAreaInsets.bottom,
+            width: view.width-40,
+            height: 50)
+        
+    }
+    
+    @objc func didTapSignIn() {
+        pushNewVC()
+    }
+    
+    private func pushNewVC() {
+        let authVC = AuthViewController()
+        authVC.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(authVC, animated: true)
     }
 
 }
