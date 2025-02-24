e0465f820393a11e081cc7f838a9a2b931027149
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-02-24 23:06:48 +0200

    API caller func to get current user profile

--- Spotify/Managers/APICaller.swift
+++ Spotify/Managers/APICaller.swift
@@ -13,9 +13,49 @@ final class APICaller {
     
     private init() {}
     
-    // MARK: - Methods
-    public func getCurrentUserProfile(completion: @escaping (Result<UserProfile, Error>) -> Void) {
-        
+    // MARK: - Enum
+    enum HTTPMethod: String {
+        case GET
+        case POST
     }
     
+    enum APIError: Error {
+        case decodingFailed
+        case failedToGetData
+    }
+    
+    // MARK: - Methods
+    public func getCurrentUserProfile(completion: @escaping (Result<UserProfile, Error>) -> Void) {
+        createRequest(url: URL(string: "\(Constants.baseAPIURL)/me"), type: .GET) { baseRequest in
+            let task = URLSession.shared.dataTask(with: baseRequest) { data, _, error in
+                guard let data = data, error == nil else {
+                    completion(.failure(APIError.failedToGetData))
+                    return
+                }
+                
+                do {
+                    let results = try JSONSerialization.jsonObject(with: data)
+                    print(results)
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+            task.resume()
+        }
+    }
+        
+    private func createRequest(url: URL?, type: HTTPMethod ,completion: @escaping (URLRequest) -> Void) {
+        AuthManager.shared.withValidTOken { token in
+            guard let apiURL = url else { return }
+            var request  = URLRequest(url: apiURL)
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorizaton")
+            request.httpMethod = type.rawValue
+            request.timeoutInterval = 30
+            completion(request)
+        }
+    }
+}
+
+struct Constants {
+    static let baseAPIURL = "https://api.spotify.com/v1"
 }
