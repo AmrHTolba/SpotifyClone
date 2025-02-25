1e33be8bdbf95d1a1083364b8a1a034dae12e40a
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-02-25 16:34:15 +0200

    UserProfile model created (not finished yet)

--- Spotify/Models/UserProfile.swift
+++ Spotify/Models/UserProfile.swift
@@ -7,6 +7,27 @@
 
 import Foundation
 
-struct UserProfile {
-    
+struct UserProfile: Codable {
+    let country, displayName, email: String
+    let explicitContent: [String: Int]
+    let externalUrls: [String: Int]
+    //let followers: [String: Codable?]
+    let id: String
+    let images: [UserImage]
+    let product: String
+
+    enum CodingKeys: String, CodingKey {
+        case country
+        case displayName = "display_name"
+        case email
+        case explicitContent = "explicit_content"
+        case externalUrls = "external_urls"
+        //case followers
+        case id, images, product
+        }
+}
+
+struct UserImage: Codable {
+    let url: String
+    let height, width: Int
 }
