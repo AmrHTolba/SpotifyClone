27324fc7e084f86b89ca1003434aff9a18ce435b
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-02-26 17:32:26 +0200

    Artist and new releases model

--- Spotify/Models/Artist.swift
+++ Spotify/Models/Artist.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct Artist: Codable {
+    let id: String
+    let name: String
+    let type: String
+    let externalUrls: [String: String]
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case type
+        case externalUrls = "external_urls"
+    }
+}
+
--- Spotify/Models/NewRealeses.swift
+++ Spotify/Models/NewRealeses.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+
+struct NewReleases: Codable {
+    let albums: [AlbumsResponse]
+    
+    enum CodingKeys: String, CodingKey {
+        case albums
+    }
+}
+
+struct AlbumsResponse: Codable {
+    let items: [Album]
+}
+
+struct Album: Codable {
+    let albumType: String
+    let artists: [Artist]
+    let availableMarkets: [String]
+    let id: String
+    let images: [APIImage]
+    let releaseDate: String
+    let totalTracks: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case albumType = "album_type"
+        case availableMarkets = "available_markets"
+        case releaseDate = "release_date"
+        case totalTracks = "total_tracks"
+        case artists, id, images
+        
+    }
+}
